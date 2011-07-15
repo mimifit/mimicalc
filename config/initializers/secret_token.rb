8c878d1930e82c810d1f0384d7663100377c4ab8
Mimi Fit <mimifit.com@gmail.com>
2011-07-14 19:29:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mimicalc::Application.config.secret_token = 'e49b9aa8e76252d82ca5f961251f4bec3568ba4f135d177a10fbf5b16d6d07430d683bae0dfdea1ba0ef352b901232dfb8f225f70260e83e5a1deb92dd993aa9'
